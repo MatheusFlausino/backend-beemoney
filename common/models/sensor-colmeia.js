02216e9ab4e42df42ac83dc6348be32f815934c1
ViniciusRomanoBit <romano@alunos.utfpr.edu.br>
2019-04-12 14:05:27 -0300

    .

--- common/models/sensor-colmeia.js
+++ common/models/sensor-colmeia.js
@@ -6,7 +6,25 @@ module.exports = function (SensorColmeia) {
         // console.log(ctx);
         var inst = ctx.instance ? ctx.instance : ctx.data;
         inst.col_id = 1;
-        next()        
+
+
+        SensorColmeia.app.models.Colmeia.findOne({include:[{
+            relation:'medicoes',
+            scope:{
+                order:'createdat ASC',
+                limit:1                
+            }
+        }]},function (err,res) {
+            if(inst.valor >= res.peso_cheio){
+                SensorColmeia.app.models.Alarme.create({tipo:1,lido:false})
+            }
+
+            if(res.__data && res.__data.medicoes && res.__data.medicoes[0] && ((res.__data.medicoes[0].valor)/1.5) > inst.valor){
+                SensorColmeia.app.models.Alarme.create({tipo:2,lido:false})
+            }
+    
+            next()        
+        })
     })
 
     SensorColmeia.on('dataSourceAttached', function (obj) {
